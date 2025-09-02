5cf76de1628191fd09bf84218b29a7064d46dd29
Rachana M R <rachanaramesh212000@gmail.com>
2025-09-02 20:40:47 +0530

    Update Pizza_Runner.sql

--- Pizza_Runner.sql
+++ Pizza_Runner.sql
@@ -1,11 +1,1 @@
-# 🍕 Pizza Runner SQL Project
 
-This project analyses Pizza Runner delivery data using SQL.  
-Database: **MySQL**
-
----
-
-## 🗄 Database Setup
-```sql
-CREATE DATABASE pizza_runner;
-USE pizza_runner;
